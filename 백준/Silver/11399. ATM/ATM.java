a967768eb23ad53055f0e8527b2c729705af5d98
seonghyeon-m <139305010+seonghyeon-m@users.noreply.github.com>
2023-07-27 09:14:16 +0900

    [Silver IV] Title: ATM, Time: 268 ms, Memory: 20284 KB -BaekjoonHub

---
+++ 백준/Silver/11399. ATM/ATM.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int N = sc.nextInt();
+		int[] p = new int[N];
+		
+		for (int i = 0; i < N; i++) {
+			p[i] = sc.nextInt();
+		}
+		
+		Arrays.sort(p);
+		
+		int sum = 0;
+		for (int i = 0; i < N; i++) {
+			sum += (N-i)*p[i];
+		}
+		
+		System.out.println(sum);
+	}
+}
