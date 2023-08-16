947c426af7f58ee685b998db739a1007ad4c6afa
seonghyeon-m <139305010+seonghyeon-m@users.noreply.github.com>
2023-08-17 01:16:03 +0900

    [Silver II] Title: 쇠막대기, Time: 240 ms, Memory: 18532 KB -BaekjoonHub

---
+++ 백준/Silver/10799. 쇠막대기/쇠막대기.java
@@ -0,0 +1,47 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+	static ArrayList<Character> stack = new ArrayList<>();
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);	
+		String str = sc.next();
+		int ans = 0;
+			
+		for (int i = 0; i < str.length(); i++) {
+			if (str.charAt(i) == 40) {
+				push(str.charAt(i));
+			} else {
+				if (str.charAt(i-1) == 40) {
+					// () 모양으로 만나는 곳: 레이저로 자르는 부분이라 (stack의 크기-1)만큼 더함
+					ans +=(stack.size()-1);
+					pop();
+				} else {
+					// )) 모양으로 만나는 곳: 막대기가 끝나는 부분이라 1만큼 더함
+					ans++;
+					pop();
+				}
+			}
+		}
+
+		System.out.println(ans);
+		
+		sc.close();
+		
+	} //main method
+	
+	static void push(char c) {
+		stack.add(c);
+	}
+	
+	static char pop() {
+		return stack.remove(stack.size()-1);
+	}
+	
+	static boolean isEmpty() {
+		if (stack.size() == 0) return true;
+		return false;
+	}
+	
+}
