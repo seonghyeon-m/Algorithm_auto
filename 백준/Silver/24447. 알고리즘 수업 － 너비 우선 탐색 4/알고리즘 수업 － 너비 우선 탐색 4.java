04bf9b5903bc50dd1cbb2754abcfbb8c2d01f1ce
seonghyeon-m <139305010+seonghyeon-m@users.noreply.github.com>
2023-09-13 17:19:38 +0900

    [Silver II] Title: 알고리즘 수업 - 너비 우선 탐색 4, Time: 1728 ms, Memory: 335008 KB -BaekjoonHub

---
+++ 백준/Silver/24447. 알고리즘 수업 － 너비 우선 탐색 4/알고리즘 수업 － 너비 우선 탐색 4.java
@@ -0,0 +1,68 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+public class Main {
+	
+	static boolean[] visited;
+	static ArrayList<Integer>[] link;
+	static Queue<Integer> queue = new LinkedList<>();
+	static long[] answer;
+	static int cnt = 0;
+	static int[] order;
+	
+	@SuppressWarnings("unchecked")
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int N = sc.nextInt(); // 정점의 수
+		int M = sc.nextInt(); // 간선의 수
+		int R = sc.nextInt()-1; // 시작 정점
+		
+		link = new ArrayList[N];
+		for (int i = 0; i < N; i++) link[i] = new ArrayList<>();
+		for (int i = 0; i < M; i++) {
+			int a = sc.nextInt()-1;
+			int b = sc.nextInt()-1;
+			link[a].add(b);
+			link[b].add(a);
+		}
+		for (int i = 0; i < N; i++) Collections.sort(link[i]);
+		
+		visited = new boolean[N];
+		answer = new long[N];
+		order = new int[N];
+		for (int i = 0; i < N; i++) answer[i] = -1;
+		visited[R] = true;
+		answer[R] = 0;
+		order[R] = ++cnt;
+		BFS(R);
+		
+		long finalAnswer = 0;
+		for (int i = 0; i < N; i++) {
+			finalAnswer +=answer[i]*order[i];
+		}
+		
+		System.out.println(finalAnswer);
+		
+		sc.close();
+	}
+	
+	public static void BFS(int now) {
+
+		for (int i = 0; i < link[now].size(); i++) {
+			if (!visited[link[now].get(i)]) {
+				queue.add(link[now].get(i));
+				visited[link[now].get(i)] = true;
+				answer[link[now].get(i)] = answer[now]+1;
+				order[link[now].get(i)] = ++cnt;
+			}
+		}
+		
+		if (!queue.isEmpty()) BFS(queue.poll());
+		else return;
+		
+	}
+}
