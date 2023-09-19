9fa662618f9e2a19949f4c28bdefca3bebac59a6
성현 <139305010+seonghyeon-m@users.noreply.github.com>
2023-09-19 11:10:41 +0900

    [Gold V] Title: 토마토, Time: 1424 ms, Memory: 310160 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.java
+++ 백준/Gold/7576. 토마토/토마토.java
@@ -1,77 +1,86 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.LinkedList;
 import java.util.Queue;
-import java.util.StringTokenizer;
+import java.util.Scanner;
 
 public class Main {
-	public static void main(String[] args) throws NumberFormatException, IOException {
-		//input
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
+	
+	static class Node {
+		int r, c, depth;
+		public Node(int r, int c, int depth) {
+			this.r = r;
+			this.c = c;
+			this.depth = depth;
+		}
+	}
+	
+	static int nowDepth;
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
 		
-		int M = Integer.parseInt(st.nextToken());
-		int N = Integer.parseInt(st.nextToken());
+		int C = sc.nextInt();
+		int R = sc.nextInt();
 		
-		int[][] box = new int[N][M];
-		int total = M*N; //tomato 총 개수
-		int cnt = 0;//익은거 개수
-		Queue<Integer> queue = new LinkedList<>();
-		for(int i = 0; i < N; i++) {
-			st = new StringTokenizer(br.readLine());
-			for(int j = 0; j < M; j++) {
-				box[i][j] = Integer.parseInt(st.nextToken());
-				if(box[i][j] == -1) {//없는칸 total에서 제거
-					total--;
-				}
-				
-				if(box[i][j] == 1) {//있으면 count, visited변경, 큐에 추가
-					cnt++;
-					queue.add(i*M + j);
-				}
+		int[][] tomato = new int[R][C];
+		for (int r = 0; r < R; r++) {
+			for (int c = 0; c < C; c++) {
+				tomato[r][c] = sc.nextInt();
 			}
 		}
 		
-		int day = 0;
-		int[] dr = {-1, 1, 0, 0};
-		int[] dc = {0, 0, -1, 1};
-		Queue<Integer> temp = new LinkedList<>();
-		while(cnt != total) {
-			day++;
-			//들어있던 것 모두 제거
-			while(!queue.isEmpty()){
-				int pos = queue.remove();
-				int r = pos / M;
-				int c = pos % M;
-				for(int d = 0; d < 4; d++) {
-					int nr = r + dr[d];
-					int nc = c + dc[d];
-					if(nr >= 0 && nr < N && nc >= 0 && nc < M && box[nr][nc] == 0) {
-						box[nr][nc] = 1;
-						temp.add(nr*M + nc);
-						cnt++;
+		int[] dr = { -1, 1, 0, 0 };
+		int[] dc = { 0, 0, -1, 1 };
+		
+		Queue<Node> queue = new LinkedList<>();
+		boolean[][] visited = new boolean[R][C];
+		int ripe = 0;
+		int empty = 0;
+		
+		for (int r = 0; r < R; r++) {
+			for (int c = 0; c < C; c++) {
+				if (tomato[r][c] == 1) {
+					queue.add(new Node(r, c, 0));
+					visited[r][c] = true;
+					ripe++;
+				} else if (tomato[r][c] == -1) {
+					empty++;
+				}
+			}
+		}
+
+		while (!queue.isEmpty()) {
+			// BFS
+			
+			Node nowNode = queue.poll();
+			int nowR = nowNode.r;
+			int nowC = nowNode.c;
+			nowDepth = nowNode.depth;
+			
+			for (int i = 0; i < 4; i++) {
+				
+				int nr = nowR + dr[i];
+				int nc = nowC + dc[i];
+				
+				if (nr >= 0 && nr < R && nc >= 0 && nc < C) {
+					if (!visited[nr][nc]) {
+						if (tomato[nr][nc] == 0) {
+							queue.add(new Node(nr, nc, nowDepth+1));
+							visited[nr][nc] = true;
+							ripe++;
+						}
 					}
 				}
-			}
-			
-			//temp가 비어있으면 더이상 불가능. break
-			if(temp.isEmpty()) {
-				break;
-			}
-			
-			
-			//temp에서 queue로 이전
-			while(!temp.isEmpty()) {
-				queue.add(temp.remove());
-			}
-		}
+				
+				
+			} //for delta탐색
+
+		} //while
 		
-		if(cnt != total) day=-1;
-		
-		System.out.println(day);
-		
-		
-		
-	}//end of main method
-}//end of class
+		if (ripe == R*C-empty)
+			System.out.println(nowDepth);
+		else
+			System.out.println(-1);
+
+		sc.close();
+	}
+}
