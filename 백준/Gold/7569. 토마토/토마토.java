886f6a52f7aef80a42c647095a8342c9746b0086
성현 <139305010+seonghyeon-m@users.noreply.github.com>
2023-09-19 11:15:57 +0900

    [Gold V] Title: 토마토, Time: 1488 ms, Memory: 313772 KB -BaekjoonHub

--- 백준/Gold/7569. 토마토/토마토.java
+++ 백준/Gold/7569. 토마토/토마토.java
@@ -1,89 +1,96 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Stack;
-import java.util.StringTokenizer;
-
-/*알고리즘 설명
- * 1. 그래프 탐색
- * 2. DFS 이용
- * 3. 인자로 탐색 시작한 정점 번호와 현재 DFS 탐색 중인 정점 번호 입력
- * 4. 연결된 노드 있으면 재귀식으로 호출, 탐색 시작점 기준 미방문 노드일 경우만 재귀호출
- * 5. 결과 출력
- * */
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
 
 public class Main {
 
-	
-	public static void main(String[] args) throws IOException {
-		//input, 초기화
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		
-		int M = Integer.parseInt(st.nextToken());
-		int N = Integer.parseInt(st.nextToken());
-		int H = Integer.parseInt(st.nextToken());
-		
-		int[][][] box = new int[N][M][H];
-		int total = N*M*H;
-		int now = 0;
-		//위치 정보 (현재높이*(N*M) + 현재행*(M) + 현재 열)
-		Stack<Integer> stack = new Stack<>();
-		for(int i = 0; i < H; i++) {
-			for(int j = 0; j < N; j++) {
-				st = new StringTokenizer(br.readLine());
-				for(int k = 0; k < M; k++) {
-					box[j][k][i] = Integer.parseInt(st.nextToken());
-					if(box[j][k][i] == -1) {//토마토 총 개수 구하기
-						total--;
-					}
-					if(box[j][k][i] == 1) {
-						stack.add(i*(N*M) + j*M + k);
-						now++;
-					}
-				}
-			}
-		}
-		//delta
-		int[] dr = {-1, 1, 0, 0, 0, 0};
-		int[] dc = {0, 0, -1, 1, 0, 0};
-		int[] dh = {0, 0, 0, 0, -1, 1};
-		int day = 0;
-		Stack<Integer> temp = new Stack<>();
-		while(now != total) {
-			day++;
-			while(!stack.isEmpty()) {
-				int pos = stack.pop();
-				int height = pos / (N*M);
-				pos -= (N*M)*height;
-				int row = pos / M;
-				int col = pos % M;
-				
-				for(int d = 0; d < 6; d++) {
-					int nr = row + dr[d];
-					int nc = col + dc[d];
-					int nh = height + dh[d];
-					if(nr >= 0 && nr < N && nc >= 0 && nc < M && nh >= 0 && nh < H && box[nr][nc][nh] == 0) {
-						box[nr][nc][nh] = 1;
-						now++;
-						temp.add(nh*(N*M) + nr*M + nc);
-					}
-				}
-			}	
-			if(temp.isEmpty()) {
-				break;
-			}
-				
-			while(!temp.isEmpty()) {
-				stack.add(temp.pop());
-			}
-		}
-		
-		if(now != total) {
-			System.out.println(-1);
-		} else {
-			System.out.println(day);
+	static class Node {
+		int h, r, c, depth;
+
+		public Node(int h, int r, int c, int depth) {
+			this.h = h;
+			this.r = r;
+			this.c = c;
+			this.depth = depth;
 		}
 	}
 
+	static int nowDepth;
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		int C = sc.nextInt();
+		int R = sc.nextInt();
+		int H = sc.nextInt();
+
+		int[][][] tomato = new int[H][R][C];
+		for (int h = 0; h < H; h++) {
+			for (int r = 0; r < R; r++) {
+				for (int c = 0; c < C; c++) {
+					tomato[h][r][c] = sc.nextInt();
+				}
+			}
+		}
+
+		int[] dh = { -1, 1, 0, 0, 0, 0 };
+		int[] dr = { 0, 0, -1, 1, 0, 0 };
+		int[] dc = { 0, 0, 0, 0, -1, 1 };
+
+		Queue<Node> queue = new LinkedList<>();
+		boolean[][][] visited = new boolean[H][R][C];
+		int ripe = 0;
+		int empty = 0;
+		
+		for (int h = 0; h < H; h++) {
+			for (int r = 0; r < R; r++) {
+				for (int c = 0; c < C; c++) {
+					if (tomato[h][r][c] == 1) {
+						queue.add(new Node(h, r, c, 0));
+						visited[h][r][c] = true;
+						ripe++;
+					} else if (tomato[h][r][c] == -1) {
+						empty++;
+					}
+				}
+			}
+		}
+		
+
+		while (!queue.isEmpty()) {
+			// BFS
+
+			Node nowNode = queue.poll();
+			int nowH = nowNode.h;
+			int nowR = nowNode.r;
+			int nowC = nowNode.c;
+			nowDepth = nowNode.depth;
+
+			for (int i = 0; i < 6; i++) {
+				
+				int nh = nowH + dh[i];
+				int nr = nowR + dr[i];
+				int nc = nowC + dc[i];
+
+				if (nh >= 0 && nh < H && nr >= 0 && nr < R && nc >= 0 && nc < C) {
+					if (!visited[nh][nr][nc]) {
+						if (tomato[nh][nr][nc] == 0) {
+							queue.add(new Node(nh, nr, nc, nowDepth + 1));
+							visited[nh][nr][nc] = true;
+							ripe++;
+						}
+					}
+				}
+
+			} // for delta탐색
+
+		} // while
+
+		if (ripe == H * R * C - empty)
+			System.out.println(nowDepth);
+		else
+			System.out.println(-1);
+
+		sc.close();
+	}
 }
