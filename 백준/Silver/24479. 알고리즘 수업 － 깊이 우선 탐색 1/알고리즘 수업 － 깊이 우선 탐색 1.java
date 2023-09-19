ec088a26e5497cb03c0fd26172e7ef5d9fb9e752
성현 <139305010+seonghyeon-m@users.noreply.github.com>
2023-09-19 12:18:24 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 1, Time: 1732 ms, Memory: 323436 KB -BaekjoonHub

---
+++ 백준/Silver/24479. 알고리즘 수업 － 깊이 우선 탐색 1/알고리즘 수업 － 깊이 우선 탐색 1.java
@@ -0,0 +1,55 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+
+public class Main {
+
+	static boolean[] visited;
+	static ArrayList<Integer>[] link;
+	static int cnt = 0;
+	static int[] order;
+	
+	@SuppressWarnings("unchecked")
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int N = sc.nextInt(); // 정점의 수
+		int M = sc.nextInt(); // 간선의 수
+		int R = sc.nextInt()-1; // 시작 정점
+		
+		link = new ArrayList[N];
+		for (int i = 0; i < N; i++) link[i] = new ArrayList<>();
+		for (int i = 0; i < M; i++) {
+			int a = sc.nextInt()-1;
+			int b = sc.nextInt()-1;
+			link[a].add(b);
+			link[b].add(a);
+		}
+		for (int i = 0; i < N; i++) Collections.sort(link[i]);
+		
+		visited = new boolean[N];
+		order = new int[N];
+		DFS(R);
+		
+		StringBuilder sb = new StringBuilder();
+		for (int i = 0; i < N; i++) sb.append(order[i]).append("\n");
+		System.out.println(sb);
+			
+		sc.close();
+	}
+	
+	public static void DFS(int now) {
+
+		if (!visited[now]) {
+			visited[now] = true;
+			cnt++;
+			order[now] = cnt;
+			for (int i = 0; i < link[now].size(); i++) {
+				if (!visited[link[now].get(i)]) {
+					DFS(link[now].get(i));
+				} else continue;
+			}
+		} else return;
+		
+	}
+}
