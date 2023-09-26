d31072307abefd6d6e6cbf8ca85f8f2d6c4996cf
성현 <139305010+seonghyeon-m@users.noreply.github.com>
2023-09-26 15:11:25 +0900

    [Silver I] Title: 숨바꼭질, Time: 148 ms, Memory: 23368 KB -BaekjoonHub

---
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
@@ -0,0 +1,73 @@
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+public class Main {
+	
+	static class Node {
+		int x, time;
+		public Node(int x, int time) {
+			this.x = x;
+			this.time = time;
+		}
+	}
+	
+	static Queue<Node> queue = new LinkedList<>();
+	static int N, K;
+	static boolean[] visited;
+	static int[] map;
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		N = sc.nextInt(); // 출발
+		K = sc.nextInt(); // 도착
+		
+		if (N > K) {			
+			visited = new boolean[2*N];
+			map = new int[2*N];
+		}
+		else {
+			visited = new boolean[4*K];
+			map = new int[4*K];
+		}
+		
+		System.out.println(move(new Node(N, 0)));
+		
+		sc.close();
+	}
+	
+	public static int move(Node node) {
+		
+		queue.add(new Node(N, 0));
+		
+		while (!queue.isEmpty()) {
+			Node nowNode = queue.poll();
+			
+			if (nowNode.x == K) return nowNode.time;
+			
+			int twice = 2 * nowNode.x;
+			int plus = nowNode.x + 1;
+			int minus = nowNode.x - 1;
+			
+			if (twice < 2*K)
+				if (!visited[twice]) {
+					queue.add(new Node(twice, nowNode.time+1));
+					visited[twice] = true;
+				}
+			if (plus <= K)
+				if (!visited[plus]) {
+					queue.add(new Node(plus, nowNode.time+1));
+					visited[plus] = true;
+				}
+			if (minus >= 0)
+				if (!visited[minus]) {
+					queue.add(new Node(minus, nowNode.time+1));
+					visited[minus] = true;
+				}
+		}
+		
+		return 0;
+		
+	}
+}
