25046a7ebdbe10b541e9f26a557279649fd0a440
성현 <139305010+seonghyeon-m@users.noreply.github.com>
2023-11-02 10:40:47 +0900

    [Gold III] Title: 세 용액, Time: 364 ms, Memory: 22772 KB -BaekjoonHub

--- 백준/Gold/2473. 세 용액/세 용액.java
+++ 백준/Gold/2473. 세 용액/세 용액.java
@@ -64,6 +64,24 @@ public class Main {
 			p3--;
 			p1 = 0;
 			p2 = p3-1;
+			
+			// pointer 위치 바뀌었으니 한 번 다시 확인 해줘야 함
+			if (p3 != 1 && potion[p1] + potion[p2] + potion[p3] == 0) {
+				// 만약 특성값의 합이 0이면 기록
+				// 탐색 종료
+				close = 0;
+				ans[0] = p1;
+				ans[1] = p2;
+				ans[2] = p3;
+				break Main;
+			} else if (p3 != 1 && Math.abs(potion[p1] + potion[p2] + potion[p3]) < close) {
+				// 특성값의 합이 0은 아니지만 현재까지 찾은 특성값 합보다 0에 가깝다면 기록
+				// 계속 탐색
+				close = Math.abs(potion[p1] + potion[p2] + potion[p3]);
+				ans[0] = p1;
+				ans[1] = p2;
+				ans[2] = p3;
+			}
 		}
 		
 		// 출력
