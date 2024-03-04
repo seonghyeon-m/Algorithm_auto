4fe5f33b3124a02eec024d55b9141fe644b66a0c
성현 <139305010+seonghyeon-m@users.noreply.github.com>
2024-03-05 02:24:33 +0900

    [Gold IV] Title: 녹색 옷 입은 애가 젤다지?, Time: 628 ms, Memory: 55068 KB -BaekjoonHub

--- 백준/Gold/4485. 녹색 옷 입은 애가 젤다지？/녹색 옷 입은 애가 젤다지？.java
+++ 백준/Gold/4485. 녹색 옷 입은 애가 젤다지？/녹색 옷 입은 애가 젤다지？.java
@@ -1,5 +1,4 @@
-import java.util.LinkedList;
-import java.util.Queue;
+import java.util.PriorityQueue;
 import java.util.Scanner;
 
 public class Main {
@@ -7,12 +6,18 @@ public class Main {
     static int[] dr = { -1, 1, 0, 0 };
     static int[] dc = { 0, 0, -1, 1 };
 
-    static class Index {
-        int r, c;
+    static class Index implements Comparable<Index> {
+        int r, c, dist;
 
-        public Index(int r, int c) {
+        public Index(int r, int c, int dist) {
             this.r = r;
             this.c = c;
+            this.dist = dist;
+        }
+
+        @Override
+        public int compareTo(Index o) {
+            return this.dist - o.dist;
         }
     }
 
@@ -28,41 +33,40 @@ public class Main {
         while (N > 0) {
             prob++;
 
-            int[][] black = new int[N][N]; // 각 칸의 도둑루피
-            int[][] dist = new int[N][N]; // 그동안 획득한 도둑루피의 합
+            int[][] thief = new int[N][N]; // 각 칸의 도둑루피
+            int[][] total = new int[N][N]; // 그동안 획득한 도둑루피의 합
             for (int i = 0; i < N; i++) {
                 for (int j = 0; j < N; j++) {
-                    black[i][j] = sc.nextInt();
-                    dist[i][j] = INF;
+                    thief[i][j] = sc.nextInt();
+                    total[i][j] = INF;
                 }
             }
 
             // BFS
-            Queue<Index> queue = new LinkedList<Index>();
-            dist[0][0] = black[0][0];
-            queue.offer(new Index(0, 0));
+            PriorityQueue<Index> pq = new PriorityQueue<Index>();
+            total[0][0] = thief[0][0];
+            pq.offer(new Index(0, 0, total[0][0]));
 
-            while (!queue.isEmpty()) {
-                Index idx = queue.poll();
+            while (!pq.isEmpty()) {
+                Index idx = pq.poll();
 
                 for (int n = 0; n < 4; n++) {
                     int nr = idx.r + dr[n];
                     int nc = idx.c + dc[n];
 
                     if (isRange(nr, nc, N)) {
-                        int nd = dist[idx.r][idx.c] + black[nr][nc];
-                        if (dist[nr][nc] > nd) {
+                        int newTotal = total[idx.r][idx.c] + thief[nr][nc];
+                        if (total[nr][nc] > newTotal) {
                             // 기록해둔 최단거리가 새로 구한 최단거리보다 길면 짧은것으로 갱신한다
-                            dist[nr][nc] = nd;
-                            queue.offer(new Index(nr, nc));
+                            total[nr][nc] = newTotal;
+                            pq.offer(new Index(nr, nc, total[nr][nc]));
                         }
                     }
                 }
-
             }
 
             // 출력
-            System.out.println("Problem "+prob+": "+dist[N-1][N-1]);
+            System.out.println("Problem "+prob+": "+total[N-1][N-1]);
 
             N = sc.nextInt(); // 여기서 0이 입력되면 while문 종료
         }
